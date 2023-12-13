0a2f2ec22aeee2a4dd76d69b1fce517b413663c4
rsanmarting <104087552+rsanmarting@users.noreply.github.com>
2023-12-13 02:12:35 -0300

    hfg

--- PhishingLab.py
+++ PhishingLab.py
@@ -9,7 +9,7 @@ import constants
 import react
 import time
 
-os.environ["OPENAI_API_KEY"] = st.secrets["openaikey"]["apikey"]
+os.environ["OPENAI_API_KEY"] = st.secrets.openaikey.apikey
 # Page title
 #st.set_page_config(page_title='EmailCreator')
 #st.title('ProjectP0.1')
