af70132d0875615a940c596aa1a88584ffe41560
rsanmarting <104087552+rsanmarting@users.noreply.github.com>
2023-12-13 01:10:59 -0300

    asdd

--- pages/ejemplo.py
+++ pages/ejemplo.py
@@ -6,12 +6,13 @@ from streamlit_gsheets import GSheetsConnection
 conn = st.connection("gsheets", type=GSheetsConnection)
 
 with st.form(key="datos_form"):
-        ej1=st.select_slider("EJEMPLO1",options=['1','2','3','4','5'],value=('1','1'))
+        ej1=st.select_slider("EJEMPLO1",options=[1,2,3,4,5],value=(1,1))
         ej2=st.select_slider("EJEMPLO2",options=['1','2','3','4','5'],value=('1','1'))
         ej3=st.select_slider("EJEMPLO3",options=['1','2','3','4','5'],value=('1','1'))
         ej4=st.select_slider("EJEMPLO4",options=['1','2','3','4','5'],value=('1','1'))
         ej5=st.select_slider("EJEMPLO5",options=['1','2','3','4','5'],value=('1','1'))
         ej6=st.select_slider("EJEMPLO6",options=['1','2','3','4','5'],value=('1','1'))
+        
         submit_button = st.form_submit_button(label="Submit ejemplos")
         
         if submit_button:
