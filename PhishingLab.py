71e082ee3e9ada845d26fcc11e363b46903a9284
rsanmarting <104087552+rsanmarting@users.noreply.github.com>
2023-12-13 02:02:34 -0300

    asd

--- PhishingLab.py
+++ PhishingLab.py
@@ -46,6 +46,8 @@ st.markdown("""\n""")
 
 #GSheets connection
 conn = st.connection("gsheets", type=GSheetsConnection)
+existing_data = conn.read(worksheet="datos", usecols=list(range(5)),ttl=5)
+existing_data = existing_data.dropna(how="all")
 
 # Text input
 uso_nombre = st.checkbox('Usar nombre?')
@@ -156,48 +158,32 @@ with st.form('colecting_form', clear_on_submit=True):
 if len(result):
         st.info(response)
         with st.form(key="datos_form"):
-                ej1=st.select_slider("EJEMPLO1",
-                                     options=['1','2','3','4','5'],
-                                     value=('1','1'))
-                ej2=st.select_slider("EJEMPLO2",
-                                     options=['1','2','3','4','5'],
-                                     value=('1','1'))
-                ej3=st.select_slider("EJEMPLO3",
-                                     options=['1','2','3','4','5'],
-                                     value=('1','1'))
-                ej4=st.select_slider("EJEMPLO4",
-                                     options=['1','2','3','4','5'],
-                                     value=('1','1'))
-                ej5=st.select_slider("EJEMPLO5",
-                                     options=['1','2','3','4','5'],
-                                     value=('1','1'))
-                ej6=st.select_slider("EJEMPLO6",
-                                     options=['1','2','3','4','5'],
-                                     value=('1','1'))
-
-                st.markdown("**required*")
-
+                ej1 = st.slider('Sensación de Autoridad:', 0, 5, 1)
+                ej2 = st.slider('Sensación de Urgencia:', 0, 5, 1)
+                ej3 = st.slider('Sensación de Deseo: ', 0, 5, 1)
+                ej4 = st.slider('¿Que tan probable es que creyeras el contenido del correo?', 0, 5, 1)
+                ej5 = st.slider('¿Piensas que esto podría ser peligroso en un futuro?', 0, 5, 1)
+                
                 submit_button = st.form_submit_button(label="Submit ejemplos")
-
+                
                 if submit_button:
-                        #validar
+                #validar
                         if not ej1 or not ej2:
                                 st.warning("Rellena los datos")
                         else:
                                 #crear fila
                                 ejemplo_data = pd.DataFrame(
-                                        [
-                                                {
-                                                        "Ejemplo1": ej1,
-                                                        "Ejemplo2": ej2,
-                                                        "Ejemplo3": ej3,
-                                                        "Ejemplo4": ej4,
-                                                        "Ejemplo5": ej5,
-                                                        "Ejemplo6": ej6,
-                                                }
-                                        ]
+                                [
+                                        {
+                                        "Autoridad": ej1,
+                                        "Urgencia": ej2,
+                                        "Deseo": ej3,
+                                        "CreerCorreo": ej4,
+                                        "PeligroFuturo": ej5,                  
+                                        }
+                                ]
                                 )
-
+                                updated_df = pd.concat([existing_data,ejemplo_data], ignore_index=True)
                                 #actualizar googlesheets
-                                conn.update(worksheet="datos", data=ejemplo_data)
+                                conn.update(worksheet="datos", data=updated_df)
                                 st.success("Gracias!!")
