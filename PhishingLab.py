1b3be2d7af479a67d88a397b26167d1ef22170d1
rsanmarting <104087552+rsanmarting@users.noreply.github.com>
2023-12-14 05:10:29 -0300

    Update PhishingLab.py

--- PhishingLab.py
+++ PhishingLab.py
@@ -150,32 +150,6 @@ if submitted:
                         response = react.phishing_generator(nombrep,correop,direccionp,nacimientop,telefonop,laboralp,interesp,extrap)
                         result.append(response)
                         encuestaf.info(response)
-                        ej1 = encuestaf.slider('Sensación de Autoridad:', 0, 5, 1)
-                        ej2 = encuestaf.slider('Sensación de Urgencia:', 0, 5, 1)
-                        ej3 = encuestaf.slider('Sensación de Deseo: ', 0, 5, 1)
-                        ej4 = encuestaf.slider('¿Que tan probable es que creyeras el contenido del correo?', 0, 5, 1)
-                        ej5 = encuestaf.slider('¿Piensas que esto podría ser peligroso en un futuro?', 0, 5, 1)
-                        submit_button = encuestaf.form_submit_button(label="Enviar")
-                        if submit_button and len(result):
-                                #crear fila
-                                ejemplo_data = pd.DataFrame(
-                                        [
-                                                {
-                                                "Autoridad": ej1,
-                                                "Urgencia": ej2,
-                                                "Deseo": ej3,
-                                                "CreerCorreo": ej4,
-                                                "PeligroFuturo": ej5,                  
-                                                }
-                                        ]
-                                        )
-                                updated_df = pd.concat([existing_data,ejemplo_data], ignore_index=True)
-                                #actualizar googlesheets
-                                conn.update(worksheet="datos", data=updated_df)
-                                encuestaf.success("Gracias!!", icon="✅")
-                        else:
-                                encuestaf.error("Debes generar el correo!", icon="🚨")
-                        
         else:
                 with st.spinner('Calculating...'):
                         time.sleep(1)
@@ -188,7 +162,30 @@ if submitted:
 
         
 
-
+ej1 = encuestaf.slider('Sensación de Autoridad:', 0, 5, 1)
+ej2 = encuestaf.slider('Sensación de Urgencia:', 0, 5, 1)
+ej3 = encuestaf.slider('Sensación de Deseo: ', 0, 5, 1)
+ej4 = encuestaf.slider('¿Que tan probable es que creyeras el contenido del correo?', 0, 5, 1)
+ej5 = encuestaf.slider('¿Piensas que esto podría ser peligroso en un futuro?', 0, 5, 1)
+submit_button = encuestaf.form_submit_button(label="Enviar")
                         
-
+if submit_button and len(result):
+        #crear fila
+        ejemplo_data = pd.DataFrame(
+                [
+                        {
+                        "Autoridad": ej1,
+                        "Urgencia": ej2,
+                        "Deseo": ej3,
+                        "CreerCorreo": ej4,
+                        "PeligroFuturo": ej5,                  
+                        }
+                ]
+                )
+        updated_df = pd.concat([existing_data,ejemplo_data], ignore_index=True)
+        #actualizar googlesheets
+        conn.update(worksheet="datos", data=updated_df)
+        encuestaf.success("Gracias!!", icon="✅")
+else:
+        encuestaf.error("Debes generar el correo!", icon="🚨")
         
